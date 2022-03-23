61ac9a475c9f328d256ca85ba3001ea7f77e8bd5
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-24 02:11:11 +0800

    done - binary operators

--- vm.c
+++ vm.c
@@ -36,6 +36,13 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)     \
+    do                    \
+    {                     \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b);     \
+    } while (false)
     for (;;)
     {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -58,6 +65,18 @@ static InterpretResult run()
             push(constant);
             break;
         }
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
         case OP_NEGATE:
             push(-pop());
             break;
@@ -71,6 +90,7 @@ static InterpretResult run()
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
