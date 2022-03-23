25bc2e00bac2847fc870c428c1bcb33d357555e6
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-24 01:50:33 +0800

    done - execution tracing

--- vm.c
+++ vm.c
@@ -2,6 +2,7 @@
 
 #include "common.h"
 #include "vm.h"
+#include "debug.h"
 
 VM vm;
 
@@ -19,6 +20,9 @@ static InterpretResult run()
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
     for (;;)
     {
+#ifdef DEBUG_TRACE_EXECUTION
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
         uint8_t instruction;
         switch (instruction = READ_BYTE())
         {
