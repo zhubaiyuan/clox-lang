758fa855e051a3b1202cfeaeeba566fa744cc66b
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-24 01:05:57 +0800

    done - constant instructions

--- debug.c
+++ debug.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name)
 {
@@ -9,6 +10,15 @@ void disassembleChunk(Chunk *chunk, const char *name)
     {
         offset = disassembleInstruction(chunk, offset);
     }
+}
+
+static int constantInstruction(const char *name, Chunk *chunk, int offset)
+{
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
 }
 
 static int simpleInstruction(const char *name, int offset)
@@ -23,6 +33,8 @@ int disassembleInstruction(Chunk *chunk, int offset)
     uint8_t instruction = chunk->code[offset];
     switch (instruction)
     {
+    case OP_CONSTANT:
+        return constantInstruction("OP_CONSTANT", chunk, offset);
     case OP_RETURN:
         return simpleInstruction("OP_RETURN", offset);
     default:
