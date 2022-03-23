d84803449ffd5b33207bed510e22ad8419ea4a9a
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-24 02:05:08 +0800

    done - arithmetic operation unary negation

--- vm.c
+++ vm.c
@@ -58,6 +58,9 @@ static InterpretResult run()
             push(constant);
             break;
         }
+        case OP_NEGATE:
+            push(-pop());
+            break;
         case OP_RETURN:
         {
             printValue(pop());
