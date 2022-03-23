eafcf675678522d9db23589febf6407ceab016bb
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-24 01:46:15 +0800

    done - executing instructions

--- vm.h
+++ vm.h
@@ -6,9 +6,18 @@
 typedef struct
 {
     Chunk *chunk;
+    uint8_t *ip;
 } VM;
+
+typedef enum
+{
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
 
 void initVM();
 void freeVM();
+InterpretResult interpret(Chunk *chunk);
 
 #endif
