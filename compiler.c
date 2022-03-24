75ef400017e6dfc6cc202a1b089128007d9ab02c
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-24 17:39:08 +0800

    wip - advance() reads the next token, handle syntax errors in the compiler

--- compiler.c
+++ compiler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -8,9 +9,43 @@ typedef struct
 {
     Token current;
     Token previous;
+    bool hadError;
+    bool panicMode;
 } Parser;
 
 Parser parser;
+
+static void errorAt(Token *token, const char *message)
+{
+    if (parser.panicMode)
+        return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+    if (token->type == TOKEN_EOF)
+    {
+        fprintf(stderr, " at end");
+    }
+    else if (token->type == TOKEN_ERROR)
+    {
+        // Nothing.
+    }
+    else
+    {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void error(const char *message)
+{
+    errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char *message)
+{
+    errorAt(&parser.current, message);
+}
 
 static void advance()
 {
@@ -24,10 +59,23 @@ static void advance()
     }
 }
 
+static void consume(TokenType type, const char *message)
+{
+    if (parser.current.type == type)
+    {
+        advance();
+        return;
+    }
+    errorAtCurrent(message);
+}
+
 bool compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
+    parser.hadError = false;
+    parser.panicMode = false;
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.hadError;
 }
