116ce142262a7bb398103265ca327f94e787f5a5
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-24 14:54:28 +0800

    done - support literal tokens in the scanner

--- scanner.c
+++ scanner.c
@@ -20,6 +20,11 @@ void initScanner(const char *source)
     scanner.line = 1;
 }
 
+static bool isDigit(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd()
 {
     return *scanner.current == '\0';
@@ -107,6 +112,37 @@ static void skipWhitespace()
     }
 }
 
+static Token number()
+{
+    while (isDigit(peek()))
+        advance();
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext()))
+    {
+        // Consume the ".".
+        advance();
+        while (isDigit(peek()))
+            advance();
+    }
+
+    return makeToken(TOKEN_NUMBER);
+}
+
+static Token string()
+{
+    while (peek() != '"' && !isAtEnd())
+    {
+        if (peek() == '\n')
+            scanner.line++;
+        advance();
+    }
+    if (isAtEnd())
+        return errorToken("Unterminated string.");
+    // The closing quote.
+    advance();
+    return makeToken(TOKEN_STRING);
+}
+
 Token scanToken()
 {
     skipWhitespace();
@@ -114,6 +150,8 @@ Token scanToken()
     if (isAtEnd())
         return makeToken(TOKEN_EOF);
     char c = advance();
+    if (isDigit(c))
+        return number();
     switch (c)
     {
     case '(':
@@ -150,6 +188,8 @@ Token scanToken()
     case '>':
         return makeToken(
             match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    case '"':
+        return string();
     }
     return errorToken("Unexpected character.");
 }
