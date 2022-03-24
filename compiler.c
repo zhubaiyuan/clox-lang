807abb61af9fd807747287d2828ace80dab998b1
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-24 18:50:38 +0800

    wip - parsing infix expressions

--- compiler.c
+++ compiler.c
@@ -127,6 +127,31 @@ static void endCompiler()
     emitReturn();
 }
 
+static void binary()
+{
+    TokenType operatorType = parser.previous.type;
+    ParseRule *rule = getRule(operatorType);
+    parsePrecedence((Precedence)(rule->precedence + 1));
+    switch (operatorType)
+    {
+    case TOKEN_PLUS:
+        emitByte(OP_ADD);
+        break;
+    case TOKEN_MINUS:
+        emitByte(OP_SUBTRACT);
+        break;
+    case TOKEN_STAR:
+        emitByte(OP_MULTIPLY);
+        break;
+    case TOKEN_SLASH:
+        emitByte(OP_DIVIDE);
+        break;
+    default:
+        // Unreachable.
+        return;
+    }
+}
+
 static void grouping()
 {
     expression();
