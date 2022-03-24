87e93a3978c7c3e0c29d03207a1c1cf74b712520
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-25 02:20:40 +0800

    wip - unary negation and runtime errors

--- vm.c
+++ vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "common.h"
@@ -10,6 +11,19 @@ VM vm;
 static void resetStack()
 {
     vm.stackTop = vm.stack;
+}
+
+static void runtimeError(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
 }
 
 void initVM()
@@ -31,6 +45,11 @@ Value pop()
 {
     vm.stackTop--;
     return *vm.stackTop;
+}
+
+static Value peek(int distance)
+{
+    return vm.stackTop[-1 - distance];
 }
 
 static InterpretResult run()
@@ -79,7 +98,12 @@ static InterpretResult run()
             BINARY_OP(/);
             break;
         case OP_NEGATE:
-            push(-pop());
+            if (!IS_NUMBER(peek(0)))
+            {
+                runtimeError("Operand must be a number.");
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
         case OP_RETURN:
         {
