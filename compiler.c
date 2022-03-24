2a4238ec1d2cf16efdcbc4e2edf0a0dd36967efe
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-24 22:44:20 +0800

    done - dump the chunk once the compiler finishes

--- compiler.c
+++ compiler.c
@@ -5,6 +5,10 @@
 #include "compiler.h"
 #include "scanner.h"
 
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
+
 typedef struct
 {
     Token current;
@@ -134,6 +138,12 @@ static void emitConstant(Value value)
 static void endCompiler()
 {
     emitReturn();
+#ifdef DEBUG_PRINT_CODE
+    if (!parser.hadError)
+    {
+        disassembleChunk(currentChunk(), "code");
+    }
+#endif
 }
 
 static void expression();
