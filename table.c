dc4814f06d95e3393250b6de506d7a6b18401d6b
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-25 18:15:24 +0800

    done - deleting entries

--- table.c
+++ table.c
@@ -24,11 +24,27 @@ void freeTable(Table *table)
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key)
 {
     uint32_t index = key->hash % capacity;
+    Entry *tombstone = NULL;
     for (;;)
     {
         Entry *entry = &entries[index];
-        if (entry->key == key || entry->key == NULL)
+        if (entry->key == NULL)
         {
+            if (IS_NIL(entry->value))
+            {
+                // Empty entry.
+                return tombstone != NULL ? tombstone : entry;
+            }
+            else
+            {
+                // We found a tombstone.
+                if (tombstone == NULL)
+                    tombstone = entry;
+            }
+        }
+        else if (entry->key == key)
+        {
+            // We found the key.
             return entry;
         }
         index = (index + 1) % capacity;
@@ -82,6 +98,20 @@ bool tableSet(Table *table, ObjString *key, Value value)
     entry->key = key;
     entry->value = value;
     return isNewKey;
+}
+
+bool tableDelete(Table *table, ObjString *key)
+{
+    if (table->count == 0)
+        return false;
+    // Find the entry.
+    Entry *entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key == NULL)
+        return false;
+    // Place a tombstone in the entry.
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+    return true;
 }
 
 void tableAddAll(Table *from, Table *to)
