64678dedaf1b56ab6de2b0cf8dee5e3ef1da007e
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-26 15:59:22 +0800

    done - while statements

--- vm.c
+++ vm.c
@@ -244,6 +244,12 @@ static InterpretResult run()
                 vm.ip += offset;
             break;
         }
+        case OP_LOOP:
+        {
+            uint16_t offset = READ_SHORT();
+            vm.ip -= offset;
+            break;
+        }
         case OP_RETURN:
         {
             // Exit interpreter.
