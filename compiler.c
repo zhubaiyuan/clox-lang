99e21f0b2eeeaa3355a1ec8b8a6fefad47e9c1e9
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-26 15:21:01 +0800

    done - if statements

--- compiler.c
+++ compiler.c
@@ -142,6 +142,14 @@ static void emitBytes(uint8_t byte1, uint8_t byte2)
     emitByte(byte2);
 }
 
+static int emitJump(uint8_t instruction)
+{
+    emitByte(instruction);
+    emitByte(0xff);
+    emitByte(0xff);
+    return currentChunk()->count - 2;
+}
+
 static void emitReturn()
 {
     emitByte(OP_RETURN);
@@ -156,6 +164,18 @@ static uint8_t makeConstant(Value value)
         return 0;
     }
     return (uint8_t)constant;
+}
+
+static void patchJump(int offset)
+{
+    // -2 to adjust for the bytecode for the jump offset itself.
+    int jump = currentChunk()->count - offset - 2;
+    if (jump > UINT16_MAX)
+    {
+        error("Too much code to jump over.");
+    }
+    currentChunk()->code[offset] = (jump >> 8) & 0xff;
+    currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
 static void emitConstant(Value value)
@@ -524,6 +544,16 @@ static void expressionStatement()
     emitByte(OP_POP);
 }
 
+static void ifStatement()
+{
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+    int thenJump = emitJump(OP_JUMP_IF_FALSE);
+    statement();
+    patchJump(thenJump);
+}
+
 static void printStatement()
 {
     expression();
@@ -576,6 +606,10 @@ static void statement()
     if (match(TOKEN_PRINT))
     {
         printStatement();
+    }
+    else if (match(TOKEN_IF))
+    {
+        ifStatement();
     }
     else if (match(TOKEN_LEFT_BRACE))
     {
