367e39f7817775f4c9911d7a7b85ba88b44e70cc
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-27 10:04:19 +0800

    done - native functions

--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -10,6 +11,11 @@
 #include "vm.h"
 
 VM vm;
+
+static Value clockNative(int argCount, Value *args)
+{
+    return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
 
 static void resetStack()
 {
@@ -42,12 +48,22 @@ static void runtimeError(const char *format, ...)
     resetStack();
 }
 
+static void defineNative(const char *name, NativeFn function)
+{
+    push(OBJ_VAL(copyString(name, (int)strlen(name))));
+    push(OBJ_VAL(newNative(function)));
+    tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+    pop();
+    pop();
+}
+
 void initVM()
 {
     resetStack();
     vm.objects = NULL;
     initTable(&vm.globals);
     initTable(&vm.strings);
+    defineNative("clock", clockNative);
 }
 
 void freeVM()
@@ -101,6 +117,14 @@ static bool callValue(Value callee, int argCount)
         {
         case OBJ_FUNCTION:
             return call(AS_FUNCTION(callee), argCount);
+        case OBJ_NATIVE:
+        {
+            NativeFn native = AS_NATIVE(callee);
+            Value result = native(argCount, vm.stackTop - argCount);
+            vm.stackTop -= argCount + 1;
+            push(result);
+            return true;
+        }
         default:
             // Non-callable object type.
             break;
