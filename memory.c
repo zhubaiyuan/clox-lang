297f0107210cd95ba10712d8fec01bfe34305676
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 01:09:20 +0800

    done - bound methods

--- memory.c
+++ memory.c
@@ -79,6 +79,13 @@ static void blackenObject(Obj *object)
 #endif
     switch (object->type)
     {
+    case OBJ_BOUND_METHOD:
+    {
+        ObjBoundMethod *bound = (ObjBoundMethod *)object;
+        markValue(bound->receiver);
+        markObject((Obj *)bound->method);
+        break;
+    }
     case OBJ_CLASS:
     {
         ObjClass *klass = (ObjClass *)object;
@@ -126,6 +133,9 @@ static void freeObject(Obj *object)
 #endif
     switch (object->type)
     {
+    case OBJ_BOUND_METHOD:
+        FREE(ObjBoundMethod, object);
+        break;
     case OBJ_CLASS:
     {
         ObjClass *klass = (ObjClass *)object;
