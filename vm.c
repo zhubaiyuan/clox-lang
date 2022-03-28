e80d653b71cd07e7a3659f4fa951f2eb47f8042c
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 01:14:50 +0800

    done - accessing methods

--- vm.c
+++ vm.c
@@ -146,6 +146,20 @@ static bool callValue(Value callee, int argCount)
     return false;
 }
 
+static bool bindMethod(ObjClass *klass, ObjString *name)
+{
+    Value method;
+    if (!tableGet(&klass->methods, name, &method))
+    {
+        runtimeError("Undefined property '%s'.", name->chars);
+        return false;
+    }
+    ObjBoundMethod *bound = newBoundMethod(peek(0), AS_CLOSURE(method));
+    pop();
+    push(OBJ_VAL(bound));
+    return true;
+}
+
 static ObjUpvalue *captureUpvalue(Value *local)
 {
     ObjUpvalue *prevUpvalue = NULL;
@@ -335,8 +349,11 @@ static InterpretResult run()
                 push(value);
                 break;
             }
-            runtimeError("Undefined property '%s'.", name->chars);
-            return INTERPRET_RUNTIME_ERROR;
+            if (!bindMethod(instance->klass, name))
+            {
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            break;
         }
         case OP_SET_PROPERTY:
         {
