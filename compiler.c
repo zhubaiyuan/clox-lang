95bb149fba4dbd365238981d01fb9537bbdbcc81
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 02:36:21 +0800

    done - misusing this

--- compiler.c
+++ compiler.c
@@ -74,8 +74,14 @@ typedef struct Compiler
     int scopeDepth;
 } Compiler;
 
+typedef struct ClassCompiler
+{
+    struct ClassCompiler *enclosing;
+} ClassCompiler;
+
 Parser parser;
 Compiler *current = NULL;
+ClassCompiler *currentClass = NULL;
 
 static Chunk *currentChunk()
 {
@@ -584,6 +590,11 @@ static void variable(bool canAssign)
 
 static void this_(bool canAssign)
 {
+    if (currentClass == NULL)
+    {
+        error("Can't use 'this' outside of a class.");
+        return;
+    }
     variable(false);
 }
 
@@ -740,6 +751,9 @@ static void classDeclaration()
     declareVariable();
     emitBytes(OP_CLASS, nameConstant);
     defineVariable(nameConstant);
+    ClassCompiler classCompiler;
+    classCompiler.enclosing = currentClass;
+    currentClass = &classCompiler;
     namedVariable(className, false);
     consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
     while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF))
@@ -748,6 +762,7 @@ static void classDeclaration()
     }
     consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
     emitByte(OP_POP);
+    currentClass = currentClass->enclosing;
 }
 
 static void funDeclaration()
