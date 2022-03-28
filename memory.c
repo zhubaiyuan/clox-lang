2c90039131a2b65824826ecbb8b847b0468319f9
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-28 17:50:33 +0800

    done - sweeping unused objects

--- memory.c
+++ memory.c
@@ -178,6 +178,35 @@ static void traceReferences()
     }
 }
 
+static void sweep()
+{
+    Obj *previous = NULL;
+    Obj *object = vm.objects;
+    while (object != NULL)
+    {
+        if (object->isMarked)
+        {
+            object->isMarked = false;
+            previous = object;
+            object = object->next;
+        }
+        else
+        {
+            Obj *unreached = object;
+            object = object->next;
+            if (previous != NULL)
+            {
+                previous->next = object;
+            }
+            else
+            {
+                vm.objects = object;
+            }
+            freeObject(unreached);
+        }
+    }
+}
+
 void collectGarbage()
 {
 #ifdef DEBUG_LOG_GC
@@ -185,6 +214,7 @@ void collectGarbage()
 #endif
     markRoots();
     traceReferences();
+    sweep();
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
 #endif
