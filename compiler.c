67659d5f81d5cf1cd9ff3975045716418f73512c
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 12:00:59 +0800

    done - incorrect returns in initializers

--- compiler.c
+++ compiler.c
@@ -891,6 +891,10 @@ static void returnStatement()
     }
     else
     {
+        if (current->type == TYPE_INITIALIZER)
+        {
+            error("Can't return a value from an initializer.");
+        }
         expression();
         consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
         emitByte(OP_RETURN);
