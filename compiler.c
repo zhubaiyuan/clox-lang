cb96dad4bf2d48890d91828a405d3f3d1d1fb258
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 15:08:18 +0800

    done - faster super calls

--- compiler.c
+++ compiler.c
@@ -625,8 +625,18 @@ static void super_(bool canAssign)
     consume(TOKEN_IDENTIFIER, "Expect superclass method name.");
     uint8_t name = identifierConstant(&parser.previous);
     namedVariable(syntheticToken("this"), false);
-    namedVariable(syntheticToken("super"), false);
-    emitBytes(OP_GET_SUPER, name);
+    if (match(TOKEN_LEFT_PAREN))
+    {
+        uint8_t argCount = argumentList();
+        namedVariable(syntheticToken("super"), false);
+        emitBytes(OP_SUPER_INVOKE, name);
+        emitByte(argCount);
+    }
+    else
+    {
+        namedVariable(syntheticToken("super"), false);
+        emitBytes(OP_GET_SUPER, name);
+    }
 }
 
 static void this_(bool canAssign)
