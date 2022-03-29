6645699a2b73c9b07d613bd551f6b5f33853c14d
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 14:31:03 +0800

    done - executing inheritance

--- vm.c
+++ vm.c
@@ -557,6 +557,15 @@ static InterpretResult run()
         case OP_CLASS:
             push(OBJ_VAL(newClass(READ_STRING())));
             break;
+        case OP_INHERIT:
+        {
+            Value superclass = peek(1);
+            ObjClass *subclass = AS_CLASS(peek(0));
+            tableAddAll(&AS_CLASS(superclass)->methods, &subclass->methods);
+            // Subclass.
+            pop();
+            break;
+        }
         case OP_METHOD:
             defineMethod(READ_STRING());
             break;
