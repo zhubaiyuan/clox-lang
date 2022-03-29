357d094f036245747004e5c048ef8a6a3f631125
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 15:32:39 +0800

    wip - nil, true and false

--- value.h
+++ value.h
@@ -11,12 +11,24 @@ typedef struct ObjString ObjString;
 #ifdef NAN_BOXING
 #define QNAN ((uint64_t)0x7ffc000000000000)
 
+#define TAG_NIL 1   // 01.
+#define TAG_FALSE 2 // 10.
+#define TAG_TRUE 3  // 11.
+
 typedef uint64_t Value;
 
+// #define IS_BOOL(value) (((value) | 1) == TRUE_VAL)
+#define IS_BOOL(v) ((v) == TRUE_VAL || (v) == FALSE_VAL)
+#define IS_NIL(value) ((value) == NIL_VAL)
 #define IS_NUMBER(value) (((value)&QNAN) != QNAN)
 
+#define AS_BOOL(value) ((value) == TRUE_VAL)
 #define AS_NUMBER(value) valueToNum(value)
 
+#define BOOL_VAL(b) ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL ((Value)(uint64_t)(QNAN | TAG_NIL))
 #define NUMBER_VAL(num) numToValue(num)
 
 static inline double valueToNum(Value value)
