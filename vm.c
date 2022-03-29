d16f63957951f2c313eae927b8c2cf89cc4a434d
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 12:12:40 +0800

    done - invoking fields

--- vm.c
+++ vm.c
@@ -185,6 +185,12 @@ static bool invoke(ObjString *name, int argCount)
         return false;
     }
     ObjInstance *instance = AS_INSTANCE(receiver);
+    Value value;
+    if (tableGet(&instance->fields, name, &value))
+    {
+        vm.stackTop[-argCount - 1] = value;
+        return callValue(value, argCount);
+    }
     return invokeFromClass(instance->klass, name, argCount);
 }
 
