444cfb8d4f486a63aaff02ff66ce18920daa4900
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 15:25:23 +0800

    wip - conditional suppport

--- value.h
+++ value.h
@@ -6,6 +6,9 @@
 typedef struct Obj Obj;
 typedef struct ObjString ObjString;
 
+#ifdef NAN_BOXING
+typedef uint64_t Value;
+#else
 typedef enum
 {
     VAL_BOOL,
@@ -38,6 +41,7 @@ typedef struct
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 #define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj *)object}})
+#endif
 
 typedef struct
 {
