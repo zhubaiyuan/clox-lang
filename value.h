11db87eed555ab32d726c9fd0bef9112282392a2
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-29 15:34:29 +0800

    wip - objects

--- value.h
+++ value.h
@@ -9,6 +9,7 @@ typedef struct Obj Obj;
 typedef struct ObjString ObjString;
 
 #ifdef NAN_BOXING
+#define SIGN_BIT ((uint64_t)0x8000000000000000)
 #define QNAN ((uint64_t)0x7ffc000000000000)
 
 #define TAG_NIL 1   // 01.
@@ -21,15 +22,18 @@ typedef uint64_t Value;
 #define IS_BOOL(v) ((v) == TRUE_VAL || (v) == FALSE_VAL)
 #define IS_NIL(value) ((value) == NIL_VAL)
 #define IS_NUMBER(value) (((value)&QNAN) != QNAN)
+#define IS_OBJ(value) (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
 
 #define AS_BOOL(value) ((value) == TRUE_VAL)
 #define AS_NUMBER(value) valueToNum(value)
+#define AS_OBJ(value) ((Obj *)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
 
 #define BOOL_VAL(b) ((b) ? TRUE_VAL : FALSE_VAL)
 #define FALSE_VAL ((Value)(uint64_t)(QNAN | TAG_FALSE))
 #define TRUE_VAL ((Value)(uint64_t)(QNAN | TAG_TRUE))
 #define NIL_VAL ((Value)(uint64_t)(QNAN | TAG_NIL))
 #define NUMBER_VAL(num) numToValue(num)
+#define OBJ_VAL(obj) (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
 
 static inline double valueToNum(Value value)
 {
